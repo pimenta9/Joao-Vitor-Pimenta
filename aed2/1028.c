72940b0b5219b0478918834d56a2aefb26276df1
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-04-24 17:02:06 -0300

    Update 1028.c

--- aed2/1028.c
+++ aed2/1028.c
@@ -1,1 +1,50 @@
 #include <stdio.h>
+
+/**
+ * @brief Find the greatest common divisor (GCD) between two non-negative integers.
+ * Don't use x = 0 and y = 0 at the same time! At least one of the variables has to have a positive value.
+ * 
+ * @param int x
+ * @param int y 
+ * 
+ * @return GCD(x, y)
+ */
+int GCD(int x, int y);
+
+int main()
+{
+    int n, x, y;
+
+    scanf("%d", &n);
+    
+    while (n > 0)
+    {
+        scanf("%d%d", &x, &y);
+
+        printf("%d\n", GCD(x, y));
+
+        n--;
+    }
+
+    return 0;
+}
+
+int GCD(int x, int y)
+{
+    int greatest, smallest;
+
+    if (x > y)
+    {
+        greatest = x;
+        smallest = y;
+    }
+    else
+    {
+        smallest = x;
+        greatest = y;
+    }
+
+    if (smallest == 0) return greatest;
+
+    else GCD(smallest, greatest%smallest);
+}
