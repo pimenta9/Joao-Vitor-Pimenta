6d44ce4c2053d52df6beaca9c01919f00d1f82f2
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-05-05 16:45:52 -0300

    Update BST_inorder_print.c

--- Binary Search Tree/Print BST/BST_inorder_print.c
+++ Binary Search Tree/Print BST/BST_inorder_print.c
@@ -1,1 +1,19 @@
+/*
+Print the BST using the in-order traversal.
+node -> adress of the pointer to the BST's root
+*/
+void BST_inorder_print(Root** node)
+{
+    if((*node) == NULL)
+        return;
+    else
+    {
+        BST_inorder_print(&((*node)->left));
+        
+        printf(" %d", (*node)->data);
 
+        BST_inorder_print(&((*node)->right));
+
+        return;
+    }
+}
