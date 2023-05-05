84aa2250f5f6ef29cc7e3c3d78a47e452c61eeca
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-05-05 16:43:26 -0300

    Update BST_preorder_print.c

--- Binary Search Tree/Print BST/BST_preorder_print.c
+++ Binary Search Tree/Print BST/BST_preorder_print.c
@@ -1,1 +1,19 @@
+/*
+Print the BST using the pre-order traversal.
+node -> the adress of the pointer to the BST's root
+*/
+void BST_preorder_print(Root** node)
+{
+    if((*node) == NULL)
+        return;
+    else
+    {
+        printf(" %d", (*node)->data);
 
+        BST_preorder_print(&((*node)->left));
+
+        BST_preorder_print(&((*node)->right));
+
+        return;
+    }
+}
