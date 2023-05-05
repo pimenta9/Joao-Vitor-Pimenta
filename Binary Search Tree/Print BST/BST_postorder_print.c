b5d45767c382c7baa96ceb06d597be9f2cce788b
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-05-05 16:49:21 -0300

    Update BST_postorder_print.c

--- Binary Search Tree/Print BST/BST_postorder_print.c
+++ Binary Search Tree/Print BST/BST_postorder_print.c
@@ -1,1 +1,19 @@
+/*
+Print the BST using the post-order traversal.
+node -> adress of the pointer to the BST's root
+*/
+void BST_postorder_print(Root** node)
+{
+    if((*node) == NULL)
+        return;
+    else
+    {
+        BST_postorder_print(&((*node)->left));
 
+        BST_postorder_print(&((*node)->right));
+        
+        printf(" %d", (*node)->data);
+
+        return;
+    }
+}
