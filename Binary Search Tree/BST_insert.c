a6d7a2eb2debc925ea52ea9d20637f1b4a8e708b
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-05-07 19:33:26 -0300

    Update BST_insert.c

--- Binary Search Tree/BST_insert.c
+++ Binary Search Tree/BST_insert.c
@@ -1,3 +1,11 @@
+typedef struct Root
+{
+    int data;
+    struct Root *right;
+    struct Root *left;
+
+} Root;
+
 /*
 Inserts a number to the Binary Search Tree.
 root -> the adress of the pointer to the BST's root
