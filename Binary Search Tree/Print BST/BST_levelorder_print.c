93d2a56e2f36f9bcb542891a15bbad9c14cdf0f9
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-05-08 21:08:31 -0300

    Update BST_levelorder_print.c

--- Binary Search Tree/Print BST/BST_levelorder_print.c
+++ Binary Search Tree/Print BST/BST_levelorder_print.c
@@ -1,1 +1,24 @@
+/**
+ * @brief Print the elements on the Binary Search Tree using
+ * level order traversal.
+ * Needs a queue to be functional.
+ * 
+ * @param root pointer to the BST's root (top element).
+ */
+void BST_levelorder_print(Root *root)
+{
+  Queue *head = (Queue *) malloc(sizeof(Queue));
 
+  head->node = root;
+  head->next = NULL;
+
+  while (head != NULL)
+  {
+      printf("%d ", (head->node)->data);
+
+      queue_push(head, &(head->node)->left);
+      queue_push(head, &(head->node)->right);
+
+      queue_pop(&head);
+  }
+}
