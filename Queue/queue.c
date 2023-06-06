2d0f03d2bb603b18ce77e1936779b0b24b2b9ade
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-06-06 16:24:51 -0300

    Create queue.c

---
+++ Queue/queue.c
@@ -0,0 +1,47 @@
+/**
+ * @brief Pushes an element to the end of the queue
+ * 
+ * @param queue pointer to the queue
+ * @param num element
+ */
+Queue* queue_push(Queue *queue, int num)
+{  
+    Queue* new = (Queue*) malloc(sizeof(Queue));
+    if (new)
+    {
+        new->data = num;
+        new->next = NULL;
+
+        if (queue == NULL)
+        {
+            return new;
+        }
+        else
+        {
+            Queue* aux = queue;
+            while (aux->next != NULL)
+                aux = aux->next;
+            aux->next = new;
+            return queue;
+        }
+    }
+    else
+    {
+        printf("ERROR");
+    }
+}
+
+Queue* queue_pop(Queue *queue)
+{
+    if (queue->next == NULL)
+    {
+        free(queue);
+        return NULL;
+    }
+    else
+    {
+        Queue* aux = queue->next;
+        free(queue);
+        return aux;
+    }
+}
