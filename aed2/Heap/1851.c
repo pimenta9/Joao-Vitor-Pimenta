af1b15cb165ddc68003d19189ee1d892efc69ca1
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-06-14 13:50:37 -0300

    Update 1851.c

--- aed2/Heap/1851.c
+++ aed2/Heap/1851.c
@@ -9,11 +9,96 @@ typedef struct dragons
     int fine;
 } Dragons;
 
-typedef struct list
+Dragons* create_maxHeap()
 {
-    Dragons dragon;
-    struct list* next;
-} List;
+    Dragons* heap = (Dragons*) malloc(sizeof(Dragons));
+    if (heap == NULL) printf("ERROR!");
+    heap[0].days = -1;
+    heap[0].fine = -1;
+
+    return heap;
+}
+
+void swap(Dragons* heap, int i, int j)
+{
+    Dragons temp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = temp;
+}
+
+Dragons* maxHeap_insert(Dragons* heap, int heapSize, Dragons x, int currentFine)
+{
+    heap = (Dragons*) realloc(heap, (heapSize+1)*sizeof(int));
+
+    heap[heapSize] = x;
+
+    int i = heapSize;
+
+    while (i > 1)
+    {
+        if (heap[i].days*currentFine < heap[i/2].days*(currentFine - heap[i/2].fine + heap[i].fine))
+        {
+            swap(heap, i, (i/2));
+            i = i/2;
+        }
+        else
+            return heap;
+    }
+    return heap;
+}
+
+Dragons* add_dragon(Dragons* heap, int heapSize, Dragons dragon, int currentFine)
+{
+    // primeiro dragão
+    if (heap[0].days == -1)
+    {
+        heap[0] = dragon;
+
+        return heap;
+    }
+    
+    return maxHeap_insert(heap, heapSize, dragon, currentFine);
+}
+
+Dragons* deleteDragon(Dragons* heap, int heapSize, int currentFine)
+{
+    Dragons temp = heap[1];
+    heap[1] = heap[heapSize-1];
+    heap[heapSize-1] = temp;
+
+    heap = (Dragons*) realloc(heap, (heapSize-1)*sizeof(Dragons));
+    heapSize--;
+
+    int i = 1;
+    while (i*2 <= (heapSize-1))
+    {
+        if ((i*2 + 1) <= heapSize-1)
+        {
+            if (heap[i].days*(currentFine-heap[i].fine) > heap[i*2].days*(currentFine - heap[i*2].fine)
+             || heap[i].days*(currentFine-heap[i].fine) > heap[i*2+1].days*(currentFine - heap[i*2+1].fine))
+            {
+                if (heap[i*2].days*(currentFine - heap[i*2].fine) < heap[i*2+1].days*(currentFine - heap[i*2+1].fine))
+                {
+                    swap(heap, i, (i*2));
+                    i = (i*2);
+                }
+                else
+                {
+                    swap(heap, i, (i*2 + 1));
+                    i = (i*2 + 1);
+                }
+            }
+            else
+                return heap;
+        }
+        else
+            if (heap[i].days*(currentFine-heap[i].fine) > heap[i*2].days*(currentFine - heap[i*2].fine))
+                swap(heap, i, (i*2));
+            else
+                return heap;
+    }
+    return heap;
+}
 
 int main ()
 {
@@ -30,20 +115,27 @@ int main ()
         dragons[n-1].fine = x.fine;
     }
 
-    List* list = NULL;
+    Dragons* heap = create_maxHeap();
     int totalFine = 0;
+    int heapSize = 0;
+    int currentFine = 0;
 
     for (int i = 0; true; i++)
     {
         // if there are still dragons to be added
         if (i < n)
-            list = add_dragon(list, dragons[i]);
+        {
+            heap = add_dragon(heap, heapSize, dragons[i], currentFine);
+            currentFine += dragons[i].fine;
+            if (i)
+                heapSize++;
+        }
 
-        (list->dragon).days--;
+        heap[0].days--;
 
         // se não tiver nenhum dragão pagando multa
         // e não for o primeiro dia, a contagem acaba
-        if (list->next == NULL)
+        if (heapSize == 1)
         {
             if (i)
                 break;
@@ -51,11 +143,15 @@ int main ()
         // caso contrário adiciona o valor da multa
         // de todos os dragões que não estão sendo treinados
         else
-            totalFine += dayFine(list->next);
+            totalFine += currentFine;
 
-        if ((list->dragon).days == 0)
-            list = deleteDragon(list);
-        if (list == NULL)
+        if (heap[0].days == 0)
+        {
+            heap = deleteDragon(heap, heapSize, currentFine);
+            currentFine -= heap[1].fine;
+            heapSize--;
+        }
+        if (heap == NULL)
             break;
     }
     printf("%d\n", totalFine);
