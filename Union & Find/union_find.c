db388e35b348a3b4effee8ebc660582ee7840483
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-06-24 15:38:17 -0300

    Update union_find.c

--- Union & Find/union_find.c
+++ Union & Find/union_find.c
@@ -1,10 +1,28 @@
+/**
+ * @brief find the root of the subset x belongs to
+ * 
+ * @param x 
+ * @param parent 
+ * @return int 
+ */
 int find (int x, int parent[])
 {
+    // if parent[x] is a value less than zero,
+    // x is parent of itself
     if (parent[x] < 0) return x;
     else
         return parent[x] = find(parent[x], parent);
 }
 
+/**
+ * @brief merge the two subsets which contain x and y;
+ * the subset with the greater amount of elements becomes
+ * "the parent" of the other one
+ * 
+ * @param parent 
+ * @param x 
+ * @param y 
+ */
 void Union (int parent[], int x, int y)
 {
     x = find(x, parent);
@@ -20,4 +38,16 @@ void Union (int parent[], int x, int y)
         parent[x] += parent[y];
         parent[y] = x;
     }
+}
+
+/**
+ * @brief initializes the array with -1,
+ * meaning every element points to itself
+ * 
+ * @param parent 
+ * @param n 
+ */
+void build (int parent[], int n)
+{
+    for (int i = 0; i <= n; i++) parent[i] = -1;
 }
