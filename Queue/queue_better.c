09d1bf0388e09bbda6c1eba704143c8c13862822
João Vitor <109254917+pimenta9@users.noreply.github.com>
2023-08-06 00:30:01 -0300

    Update queue_better.c

--- Queue/queue_better.c
+++ Queue/queue_better.c
@@ -4,7 +4,6 @@
 typedef struct Node
 {
     int i;
-    int j;
     struct Node* next;
 } node;
 
@@ -14,11 +13,18 @@ typedef struct Queue
     node* tail;
 } queue;
 
-queue* enqueue (queue* q, int i, int j)
+/**
+ * @brief insert an element on the end of the queue
+ * 
+ * @param q pointer to the queue
+ * @param i element to be inserted
+ * @return pointer to the queue after element has
+ * been inserted
+ */
+queue* enqueue (queue* q, int i)
 {
     node* new = malloc (sizeof(node));
     new->i = i;
-    new->j = j;
     new->next = NULL;
 
     if (q->head == NULL)
@@ -34,6 +40,13 @@ queue* enqueue (queue* q, int i, int j)
     return q;
 }
 
+/**
+ * @brief pops the first element of the queue
+ * 
+ * @param q pointer to the queue
+ * @return pointer to the queue after the element on the top
+ * has been popped
+ */
 queue* dequeue (queue* q)
 {
     node* aux = q->head;
@@ -47,14 +60,55 @@ queue* dequeue (queue* q)
     return q;
 }
 
-void print_queue (node* head)
+/**
+ * @brief prints the elements of the queue
+ * 
+ * @param q pointer to the queue
+ */
+void print_queue (queue* q)
 {
-    node* aux = head;
+    node* aux = q->head;
 
     while (aux != NULL)
     {
-        printf ("[ %d / %d ] -> ", aux->i, aux->j);
+        printf ("[ %d ] -> ", aux->i);
         aux = aux->next;
     }
     printf ("//\n");
+}
+
+/**
+ * @brief creates a new queue
+ * 
+ * @return the pointer to the new queue
+ */
+queue* new_queue ()
+{
+    queue* new = malloc (sizeof(queue));
+    new->head = NULL;
+    new->tail = NULL;
+
+    return new;
+}
+
+// testing...
+int main ()
+{
+    queue* q = new_queue();
+
+    q = enqueue (q, 2);
+    q = enqueue (q, 4);
+    q = enqueue (q, 7);
+    q = enqueue (q, 1);
+    q = enqueue (q, 9);
+
+    print_queue (q);
+
+    q = dequeue (q);
+    q = dequeue (q);
+    q = dequeue (q);
+
+    print_queue (q);
+
+    return 0;
 }
